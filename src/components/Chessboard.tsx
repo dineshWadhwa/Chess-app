7f3968ad7a30919aca3297c357a85e83f305bdbc
Dinesh Wadhwa <dinesh.wadhwa@capanicus.com>
2024-05-30 18:52:12 +0530

    add pieces in board

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -8,6 +8,10 @@ import BlackbishopImg from "../assets/Images/bishop_b.png";
 import WhitebishopImg from "../assets/Images/bishop_w.png";
 import BlackknightImg from "../assets/Images/knight_b.png";
 import WhiteknightImg from "../assets/Images/knight_w.png";
+import WhitekingImg from "../assets/Images/king_w.png";
+import BlackkingImg from "../assets/Images/king_b.png";
+import WhitequeenImg from "../assets/Images/queen_w.png";
+import BlackqueenImg from "../assets/Images/queen_b.png";
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
@@ -20,33 +24,35 @@ interface Piece {
 const pieces: Piece[] = [];
 
 for (let i = 0; i < 8; i++) {
-  pieces?.push({ image: BlackPawnImg, x: i, y: 6 });
+  pieces?.push({ image: `assets/Images/pawn_b.png`, x: i, y: 6 });
 }
 
 for (let i = 0; i < 8; i++) {
-  pieces?.push({ image: WhitePawnImg, x: i, y: 1 });
+  pieces?.push({ image: `assets/Images/pawn_w.png`, x: i, y: 1 });
 }
 
-pieces?.push({ image: BlackrookImg, x: 0, y: 7 });
-pieces?.push({ image: BlackrookImg, x: 7, y: 7 });
+for (let i = 0; i < 2; i++) {
+  const type = i === 0 ? 'b':'w'
+  const y = i === 0 ? 7:0
+  pieces?.push({ image: `assets/Images/rook_${type}.png`, x: 0, y });
+  pieces?.push({ image: `assets/Images/rook_${type}.png`, x: 7, y });
+  pieces?.push({ image: `assets/Images/bishop_${type}.png`, x: 2, y });
+  pieces?.push({ image: `assets/Images/bishop_${type}.png`, x: 5, y });
+  pieces?.push({ image: `assets/Images/knight_${type}.png`, x: 1, y });
+  pieces?.push({ image: `assets/Images/knight_${type}.png`, x: 6, y });
+  pieces?.push({ image: `assets/Images/king_${type}.png`, x: 3, y });
+  pieces?.push({ image: `assets/Images/queen_${type}.png`, x: 4, y });
+}
 
-pieces?.push({ image: WhiterookImg, x: 7, y: 0 });
-pieces?.push({ image: WhiterookImg, x: 0, y: 0 });
 
-pieces?.push({ image: BlackbishopImg, x: 2, y: 7 });
-pieces?.push({ image: BlackbishopImg, x: 5, y: 7 });
-
-pieces?.push({ image: WhitebishopImg, x: 2, y: 0 });
-pieces?.push({ image: WhitebishopImg, x: 5, y: 0 });
-
-pieces?.push({ image: BlackknightImg, x: 1, y: 7 });
-pieces?.push({ image: BlackknightImg, x: 6, y: 7 });
-
-pieces?.push({ image: WhiteknightImg, x: 1, y: 0 });
-pieces?.push({ image: WhiteknightImg, x: 6, y: 0 });
-
-pieces?.push({ image: WhiteknightImg, x: 1, y: 0 });
-pieces?.push({ image: WhiteknightImg, x: 6, y: 0 });
+// pieces?.push({ image: WhitebishopImg, x: 5, y: 0 });
+// pieces?.push({ image: WhitebishopImg, x: 2, y: 0 });
+// pieces?.push({ image: WhiterookImg, x: 7, y: 0 });
+// pieces?.push({ image: WhiterookImg, x: 0, y: 0 });
+// pieces?.push({ image: WhiteknightImg, x: 1, y: 0 });
+// pieces?.push({ image: WhiteknightImg, x: 6, y: 0 });
+// pieces?.push({ image: WhitekingImg, x: 4, y: 0 });
+// pieces?.push({ image: WhitequeenImg, x: 3, y: 0 });
 
 
 const Chessboard = () => {
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -9,13 +9,13 @@ export default function Tile({ number, image }: TileProps) {
   if (number % 2 === 0) {
     return (
       <div className="tile black-tile">
-        <img src={image}  />
+        <img src={image} />
       </div>
     );
   } else {
     return (
       <div className="tile white-tile">
-        <img src={image}  />
+        <img src={image} />
       </div>
     );
   }
