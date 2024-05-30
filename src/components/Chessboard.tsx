02b3d5446cc7ecdb733158922a1ff6d53eaa86d0
Dinesh Wadhwa <dinesh.wadhwa@capanicus.com>
2024-05-30 18:05:55 +0530

    add pieces in board

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,30 +1,71 @@
-import './Chessboard.css'
+import "./Chessboard.css";
+import Tile from "./Tile/Tile";
+import BlackPawnImg from "../assets/Images/pawn_b.png";
+import WhitePawnImg from "../assets/Images/pawn_w.png";
+import BlackrookImg from "../assets/Images/rook_b.png";
+import WhiterookImg from "../assets/Images/rook_w.png";
+import BlackbishopImg from "../assets/Images/bishop_b.png";
+import WhitebishopImg from "../assets/Images/bishop_w.png";
+import BlackknightImg from "../assets/Images/knight_b.png";
+import WhiteknightImg from "../assets/Images/knight_w.png";
 
-const horizontalAxis = ['a','b','c','d','e','f','g','h']
-const verticalAxis = ['1','2','3','4','5','6','7','8']
-const Chessboard = () => {
-    let board :Array <JSX.Element>= [];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-    for (let j = verticalAxis.length - 1; j >= 0; j--) {
-        for (let i = 0; i < horizontalAxis.length; i++) {
-            const number = j + i + 2;
-    
-            if(number % 2 === 0) {
-                board.push(
-                    <div className="tile black-tile"></div>
-                  );
-            } else {
-                board.push(
-                    <div className="tile white-tile"></div>
-                  );
-            }
-        }
-      }
-  return (
-    <div id='chessboard'>
-       {board}
-    </div>
-  )
+interface Piece {
+  image: string;
+  x: number;
+  y: number;
+}
+const pieces: Piece[] = [];
+
+for (let i = 0; i < 8; i++) {
+  pieces?.push({ image: BlackPawnImg, x: i, y: 6 });
 }
 
-export default Chessboard
+for (let i = 0; i < 8; i++) {
+  pieces?.push({ image: WhitePawnImg, x: i, y: 1 });
+}
+
+pieces?.push({ image: BlackrookImg, x: 0, y: 7 });
+pieces?.push({ image: BlackrookImg, x: 7, y: 7 });
+
+pieces?.push({ image: WhiterookImg, x: 7, y: 0 });
+pieces?.push({ image: WhiterookImg, x: 0, y: 0 });
+
+pieces?.push({ image: BlackbishopImg, x: 2, y: 7 });
+pieces?.push({ image: BlackbishopImg, x: 5, y: 7 });
+
+pieces?.push({ image: WhitebishopImg, x: 2, y: 0 });
+pieces?.push({ image: WhitebishopImg, x: 5, y: 0 });
+
+pieces?.push({ image: BlackknightImg, x: 1, y: 7 });
+pieces?.push({ image: BlackknightImg, x: 6, y: 7 });
+
+pieces?.push({ image: WhiteknightImg, x: 1, y: 0 });
+pieces?.push({ image: WhiteknightImg, x: 6, y: 0 });
+
+pieces?.push({ image: WhiteknightImg, x: 1, y: 0 });
+pieces?.push({ image: WhiteknightImg, x: 6, y: 0 });
+
+
+const Chessboard = () => {
+  let board: Array<JSX.Element> = [];
+
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      const number = j + i + 2;
+      let image = undefined;
+
+      pieces?.forEach((p) => {
+        if (p.x === i && p.y === j) {
+          image = p.image;
+        }
+      });
+      board.push(<Tile number={number} image={image} key={Math.random()} />);
+    }
+  }
+  return <div id="chessboard">{board}</div>;
+};
+
+export default Chessboard;
